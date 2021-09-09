75694a7d69ee77ccd7c25a939fe4701543abda48
rachaelkim831 <63431636+rachaelkim831@users.noreply.github.com>
2021-09-08 19:50:44 -0700

    completed issue-2-view entries

--- js/data.js
+++ js/data.js
@@ -2,7 +2,22 @@
 
 var data = {
   view: 'entry-form',
-  entries: [],
+  entries: [
+    {
+      title: 'Ada Lovelace',
+      image: './images/placeholder-image-square.jpg',
+      notes: `Augusta Ada King, Countess of Lovelace was an English mathmatician and writer, chiefly known for her
+      work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine.`,
+      nextEntryID: 1
+    },
+    {
+      title: 'JavaScript',
+      image: './images/placeholder-image-square.jpg',
+      notes: `JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript
+              specification.`,
+      nextEntryID: 2
+    }
+  ],
   editing: null,
   nextEntryId: 1
 };
--- js/main.js
+++ js/main.js
@@ -1,2 +1,87 @@
 /* global data */
 /* exported data */
+var imgInput = document.querySelector('#photo-url');
+var imgPlaceholder = document.querySelector('.photoUrl');
+var formEntry = document.querySelector('#form');
+var titleInput = document.querySelector('#title');
+var notes = document.querySelector('#notes');
+
+imgInput.addEventListener('input', function (event) {
+  imgPlaceholder.setAttribute('src', event.target.value);
+});
+
+formEntry.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var entry = {
+    title: titleInput.value,
+    image: imgInput.value,
+    notes: notes.value,
+    nextEntryId: data.nextEntryId
+  };
+  data.entries.unshift(entry);
+  data.nextEntryId++;
+  imgPlaceholder.setAttribute('src', './images/placeholder-image-square.jpg');
+  formEntry.reset();
+  localStorage.setItem('data', JSON.stringify(data));
+  addEntry(entry);
+  var $masterdiv2 = document.querySelector('.masterdiv2');
+  var $masterdiv1 = document.querySelector('.masterdiv1');
+  $masterdiv2.classList.remove('hidden');
+  $masterdiv1.classList.add('hidden');
+
+  localStorage.setItem('current', 'masterdiv2');
+});
+
+// issue 2
+
+function addEntry(entry) {
+  var $list = document.createElement('li');
+  var $imgDiv = document.createElement('div');
+  var $img = document.createElement('img');
+  var $textDiv = document.createElement('div');
+  var $header = document.createElement('h3');
+  var $paragraph1 = document.createElement('p');
+  $imgDiv.setAttribute('class', 'column-half');
+  $img.setAttribute('src', entry.image);
+  $textDiv.setAttribute('class', 'column-half');
+  $header.setAttribute('class', 'entry-header');
+  var $headerText = document.createTextNode(entry.title);
+  var $textNode1 = document.createTextNode(entry.notes);
+  $list.appendChild($imgDiv);
+  $imgDiv.appendChild($img);
+  $list.appendChild($textDiv);
+  $textDiv.appendChild($header);
+  $textDiv.appendChild($paragraph1);
+  $header.appendChild($headerText);
+  $paragraph1.appendChild($textNode1);
+
+  var $ul = document.querySelector('.journal-entry');
+  $ul.appendChild($list);
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  var $masterdiv2 = document.querySelector('.masterdiv2');
+  var $masterdiv1 = document.querySelector('.masterdiv1');
+  for (const entry of data.entries) {
+    addEntry(entry);
+  }
+  var current = localStorage.getItem('current');
+  if (current === 'masterdiv1') {
+    $masterdiv1.classList.remove('hidden');
+    $masterdiv2.classList.add('hidden');
+  } else {
+    $masterdiv2.classList.remove('hidden');
+    $masterdiv1.classList.add('hidden');
+  }
+});
+
+var $newButton = document.querySelector('.new');
+$newButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  var $masterdiv2 = document.querySelector('.masterdiv2');
+  var $masterdiv1 = document.querySelector('.masterdiv1');
+  $masterdiv2.classList.add('hidden');
+  $masterdiv1.classList.remove('hidden');
+
+  localStorage.setItem('current', 'masterdiv1');
+});
